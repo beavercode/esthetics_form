1e772248df81b1b3361028baf916e7c82d58ff94
bbr <erobober@gmail.com>
2015-01-23 01:25:38 +0200

    Add config.rb for compass

---
+++ config.rb
@@ -0,0 +1,22 @@
+#require 'compass/import-once/activate' ????
+# Require any additional compass plugins here.
+
+#Folder settings
+#http_path = "/"
+#relative_assets = true         #because we're not working from the root
+
+css_dir = "css"          #where the CSS will saved
+javascripts_dir = "js"   #where the JS will saved
+sass_dir = "sass"        #where our .sass files are
+images_dir = "img"       #the folder with your images
+sourcemap = false
+
+# You can select your preferred output style here (can be overridden via the command line):
+# opt= :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+# Obviously
+preferred_syntax = :sass
