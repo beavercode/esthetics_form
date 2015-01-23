3f07d8debaa0145df796e99c3b80aa1631574c90
bbr <erobober@gmail.com>
2015-01-23 02:20:59 +0200

    Modified form "nominal and quantity" block.

--- js/cert.js
+++ js/cert.js
@@ -1,24 +1,6 @@
-jQuery(document).ready(function() {
-    //select's init
-    var params = {
-        changedEl: "#n100",
-        visRows: 6,
-        scrollArrows: true
-    };
-    cuSel(params);
-    params = {
-        changedEl: "#n200",
-        visRows: 6,
-        scrollArrows: true
-    };
-    cuSel(params);
-    params = {
-        changedEl: "#n300",
-        visRows: 6,
-        scrollArrows: true
-    };
-    cuSel(params);
-
+jQuery(document).ready(function () {
+    //selects init
+    makeSelects(['#nominal', '#quantity']);
     //accordion
     $("#cert_tabs").tabs(
         "#cert_tabs div.cert__sidebar-desc",
@@ -26,11 +8,11 @@ jQuery(document).ready(function() {
     );
 
     //date calendar
-    $.tools.dateinput.localize("ru",  {
-        months:        'Январь,Февраль,Март,Апрель,Май,Июнь,Июль,Август,Сентябрь,Октябрь,Ноябрь,Декабрь',
-        shortMonths:   'Янв,Фев,Мар,Апр,Май,Июн,Июл,Авг,Сен,Окт,Ноя,Дек',
-        days:          'Воскресенье,Понедельник,Вторник,Среда,Четверг,Пятница,Суббота',
-        shortDays:     'Вск,Пнд,Втр,Срд,Чтв,Птн,Сбт'
+    $.tools.dateinput.localize("ru", {
+        months: 'Январь,Февраль,Март,Апрель,Май,Июнь,Июль,Август,Сентябрь,Октябрь,Ноябрь,Декабрь',
+        shortMonths: 'Янв,Фев,Мар,Апр,Май,Июн,Июл,Авг,Сен,Окт,Ноя,Дек',
+        days: 'Воскресенье,Понедельник,Вторник,Среда,Четверг,Пятница,Суббота',
+        shortDays: 'Вск,Пнд,Втр,Срд,Чтв,Птн,Сбт'
     });
     $("#date").dateinput({
         format: 'dddd dd, mmmm yyyy',   // the format displayed for the user
@@ -40,22 +22,33 @@ jQuery(document).ready(function() {
         max: 90,    // max selectable day (90 days onwards)
         offset: [0, 0], // tweak the position of the calendar
         selectors: false,   // whether month/year dropdowns are shown
-        onShow: function() {
+        onShow: function () {
             var pos = $('#date').offset();
             $('#calroot').css('top', pos.top + 21).css('left', pos.left - 13);
         }
     });
 
     //validation
-    $.tools.validator.fn("[minlength]", function(input, value) {
+    $.tools.validator.fn("[minlength]", function (input, value) {
         var min = input.attr("minlength");
         return value.length >= min ? true : {
             ru: "Минимум " + min + " символ" + (min > 1 ? "ов" : "")
         };
     });
     $.tools.validator.localize("ru", {
-        ':email'    : 'Неверный формат эмейл',
+        ':email': 'Неверный формат эмейл',
         '[required]': 'Поле необходимо заполнить'
     });
     $("#cert__form").validator({lang: 'ru'});
 });
+
+function makeSelects(e) {
+    for (var i = e.length - 1; i >= 0; --i) {
+        var params = {
+            changedEl: e[i],
+            visRows: 6,
+            scrollArrows: true
+        };
+        cuSel(params);
+    }
+}
